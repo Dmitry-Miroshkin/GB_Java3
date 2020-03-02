943d03f5e1d01b6684233014e0a22d117520df8d
dmm <dmm@mail.ru>
2020-03-02 23:25:34 +0300

    Lesson 5 homework done

---
+++ java/HomeWork/Lesson5/MainClass.java
@@ -0,0 +1,31 @@
+package HomeWork.Lesson5;
+
+import java.util.concurrent.CountDownLatch;
+
+public class MainClass {
+
+    public static final int CARS_COUNT = 4;
+    public static Boolean winner = false;
+    public static final CountDownLatch cdlAllReady = new CountDownLatch(CARS_COUNT);
+
+    public static final CountDownLatch cdlFinish = new CountDownLatch(CARS_COUNT);
+
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
+        Race race = new Race(new Road(60), new Tunnel(), new Road(40));
+        Car[] cars = new Car[CARS_COUNT];
+
+        for (int i = 0; i < cars.length; i++) {
+            cars[i] = new Car(race, 20 + (int) (Math.random() * 10));
+        }
+        for (int i = 0; i < cars.length; i++) {
+            new Thread(cars[i]).start();
+        }
+
+        cdlAllReady.await();
+        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+
+        cdlFinish.await();
+        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
+    }
+}
